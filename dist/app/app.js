72f35a6982dc13f534028b4c418699d8abda9709
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-15 09:55:22 +0600

    error handle in Express

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,6 +15,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+// router
+const createRouter = express_1.default.Router();
+app.use("/", createRouter);
+createRouter.get("/api/v1/users/create-users", (req, res) => {
+    const user = req.body;
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: user,
+    });
+});
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
@@ -13,13 +33,34 @@ const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
-app.get("/", logger, (req, res) => {
-    res.send("Hello World!");
-});
+app.get("/", logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send("Hello world !");
+    }
+    catch (err) {
+        console.log(err);
+        next(err);
+    }
+}));
 app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "Successfully receive data",
     });
+});
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: "Not found",
+    });
+});
+// global error handler
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(400).json({
+            success: false,
+            message: "Fail to get data",
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,25 @@
 import express, { NextFunction, Request, Response } from "express";
+import { json } from "stream/consumers";
 const app = express();
 const port = 3000;
+
+// router
+const createRouter = express.Router();
+
+app.use("/", createRouter);
+
+createRouter.get(
+  "/api/v1/users/create-users",
+  (req: Request, res: Response) => {
+    const user = req.body;
+
+    res.json({
+      success: true,
+      message: "User created successfully",
+      data: user,
+    });
+  }
+);
 
 // parsers
 app.use(express.json());
@@ -11,16 +30,41 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-app.get("/", logger, (req: Request, res: Response) => {
-  
-  res.send("Hello World!");
-});
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send("Hello world !");
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  }
+);
 
 app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json({
     message: "Successfully receive data",
   });
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(400).json({
+    success: false,
+    message: "Not found",
+  });
+});
+
+// global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err) {
+    res.status(400).json({
+      success: false,
+      message: "Fail to get data",
+    });
+  }
 });
 
 export default app;
